b1fd318772be4fc04501fc3ddad5de6a474ab0c4
shaoshuai <i@mayuko.cn>
2019-03-15 09:37:46 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Hero-tag-mtime
+
+![](https://img.shields.io/npm/l/hexo-tag-link-card.svg)
+
+Embed a movie card on your [Hexo](https://hexo.io/) article, used [mtime](http://www.mtime.com/) API.
+
+![](https://cdn.mayuko.cn/blog/20190315091034.png)
+
+## Installation
+
+```bash
+npm install hexo-tag-mtime --save
+```
+
+## Usage
+
+Use tags:
+
+```
+{% mtime videoId %}
+```
+
+Example:
+
+```
+{% mtime 218707 %}
+```
+
+Then, it generates HTML:
+
+```html
+<div class="mtime-movie_card">
+    <div class="mtime-info_section">
+        <div class="mtime-movie_header"><img src="http://img5.mtime.cn/mt/2019/01/30/152305.14999287_1280X720X2.jpg"
+                class="mtime-locandina">
+            <h1>流浪地球</h1>
+            <h4>2019，郭帆</h4><span class="mtime-minutes">125分钟</span>
+            <p class="mtime-type">冒险，科幻，剧情</p>
+        </div>
+        <div class="mtime-movie_desc">
+            <p class="mtime-text">太阳即将毁灭，人类在地球表面建造出巨大的推进器，寻找新家园。然而宇宙之路危机四伏，为了拯救地球，为了人类能在漫长的2500年后抵达新的家园，流浪地球时代的年轻人挺身而出，展开争分夺秒的生死之战。</p>
+        </div>
+    </div>
+    <div class="mtime-blur_back" style="background: url(http://img5.mtime.cn/pi/2018/03/30/185755.98495617_1280X720X2.jpg); no-repeat fixed;background-size: cover;"></div>
+</div>
+```
+
+#### How to get the movie id?
+
+1. Open [mtime](http://www.mtime.com/) website.
+2. Search movie than you want to insert.
+3. Url is like this: `http://movie.mtime.com/218707/`.
+4. The  NUMBER is video id.
+
+## Options
+
+- css: You can customize the css link, if NOT, it will use CDN by default.
+
+CDN url is `https://cdn.mayuko.cn/css/hexo-tag-mtime.css`.
+
+By modifying the `_config.yml` file under the Hexo root directory.
+
+```yaml
+mtime:
+  css: https://cdn.mayuko.cn/css/hexo-tag-mtime.css
+```
+
+## Style
+
+The default css style used [Simone Bernabè](https://codepen.io/simoberny/):
+
+- [View in CodePen](https://codepen.io/simoberny/pen/WMMqwL)
+
+## API
+
+The Hexo plugin used unofficial API by [jokermonn](https://github.com/jokermonn/-Api/), It's Powerful! 
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,63 @@
+const util = require('hexo-util');
+const request = require('request-promise');
+const cdn = 'https://cdn.mayuko.cn/css/hexo-tag-mtime.css';
+const css = (hexo.config.mtime && hexo.config.mtime.css) ? hexo.config.mtime.css : cdn;
+
+hexo.extend.tag.register('mtime', function (args) {
+    var id = args[0];
+    return result = getMoiveInfo(id).then(res => {
+        var mtimeHeader = util.htmlTag('div', {
+            class: 'mtime-movie_header'
+        }, util.htmlTag('img', {
+            src: res.img,
+            class: 'mtime-locandina'
+        }) + util.htmlTag('h1', {}, res.name) + util.htmlTag('h4', {}, res.year + '，' + res.director) + util.htmlTag('span', {
+            class: 'mtime-minutes'
+        }, res.mins) + util.htmlTag('p', {
+            class: 'mtime-type'
+        }, res.type));
+        var mtimeDesc = util.htmlTag('div', {
+            class: 'mtime-movie_desc'
+        }, util.htmlTag('p', {
+            class: 'mtime-text'
+        }, res.story));
+        var mtimeSection = util.htmlTag('div', {
+            class: 'mtime-info_section'
+        }, mtimeHeader + mtimeDesc);
+        var mtimeBack = util.htmlTag('div', {
+            class: 'mtime-blur_back',
+            style: 'background: url(' + res.thumbnail + '); no-repeat fixed;background-size: cover;'
+        }, '');
+        var mtimeCard = util.htmlTag('div', {
+            class: 'mtime-movie_card'
+        }, mtimeSection + mtimeBack);
+        console.info('hexo-tag-mtime => Get moive SUCEESS');
+        return '<link rel="stylesheet" type="text/css" href="' + css + '" />' + mtimeCard;
+    });
+}, {
+    async: true
+});
+
+async function getMoiveInfo(id) {
+    var url = "https://ticket-api-m.mtime.cn/movie/detail.api?locationId=290&movieId=" + id;
+    return request(url).then(function (body) {
+        var res = JSON.parse(body);
+        if (res.code) {
+            var data = {
+                name: res.data.basic.name,
+                nameEn: res.data.basic.nameEn,
+                mins: res.data.basic.mins,
+                img: res.data.basic.img.replace(/http/, 'https'),
+                year: res.data.basic.releaseDate.substring(0, 4),
+                overallRating: res.data.basic.overallRating,
+                director: res.data.basic.director.name,
+                story: res.data.basic.story,
+                type: res.data.basic.type.join('，'),
+                thumbnail: (typeof res.data.basic.stageImg.list[0].imgUrl == 'undefined') ? res.data.basic.img.replace(/http/, 'https') : res.data.basic.stageImg.list[0].imgUrl.replace(/http/, 'https')
+            }
+        }
+        console.log(data);
+
+        return data;
+    });
+}
